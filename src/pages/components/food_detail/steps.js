89d90102aa5d1c9f8843c90ae400b9eb30471cbd
jusstuguldur <ttudu536@gmail.com>
2023-04-24 23:09:58 +0800

    dunno

--- src/pages/components/food_detail/steps.js
+++ src/pages/components/food_detail/steps.js
@@ -38,30 +38,5 @@ const details = [
 ];
 
 export default function Steps() {
-  return details.map((row, i) => (
-    <div className="w-full py-6 flex">
-      <div className="w-[35%] py-10 flex items-center">
-        <img
-          src={row.image}
-          className="rounded-full border-4 border-white h-[30rem] w-[30rem] -ml-16 drop-shadow-lg object-cover object-center  "
-        />
-      </div>
-      <div className="w-[65%] h-fit flex justify-center">
-        <div className="absolute w-8 h-full bg-primary z-0"></div>
-        <div className="w-[75%] py-4 bg-white rounded-3xl z-10">
-          <div className="flex px-16">
-            <div className="font-pacifico text-4xl">{row.name}</div>
-            <div className=" ml-8 mt-2 text-xl">({row.status})</div>
-            <div className="ml-4 mt-2">
-              <Star value={row.rating} className></Star>
-            </div>
-          </div>
-          <div className="w-full flex justify-center">{row.duration}</div>
-          <div className="w-full h-8 bg-primary text-white pl-16">
-            Материалууд:
-          </div>
-        </div>
-      </div>
-    </div>
-  ));
+  return details.map((row, i) => <div className="w-full py-6 flex"></div>);
 }
--- src/pages/components/home/foryou.js
+++ src/pages/components/home/foryou.js
@@ -2,7 +2,7 @@ const foryou = [
   {
     value: 69,
     desc: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum  lorem ipsum lorem ipsum ",
-    category: "healthy",
+    category: "meat",
     image:
       "https://admin.acceleratingscience.com/food/wp-content/uploads/sites/5/2020/10/istock-694177316_varietycookedmeats-1.jpg",
   },
@@ -16,7 +16,7 @@ const foryou = [
 ];
 
 export default function ForYou() {
-  return (
+  return foryou.map((row) => (
     <div>
       <div className="w-full h-8 text-2xl pl-[7%] mb-4 uppercase">for you</div>
       <div className="w-full flex justify-center">
@@ -24,40 +24,32 @@ export default function ForYou() {
         <div className="z-10 w-full pl-[15%]">
           <div className=" w-[80%] h-96 bg-white rounded-3xl drop-shadow-lg overflow-hidden flex">
             <img
-              src="https://admin.acceleratingscience.com/food/wp-content/uploads/sites/5/2020/10/istock-694177316_varietycookedmeats-1.jpg"
+              src={row.image}
               className="w-[49.85%] h-full object-cover object-center"
             />
             <div className="w-[50.15%] h-full px-8 py-4">
               <div className="w-full uppercase">collection</div>
               <div className="uppercase text-2xl mt-4 font-semibold">
-                69 best meat foods recipes
+                {row.value} best {row.category} foods recipes
               </div>
-              <div className="mt-4">
-                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                Lorem ipsum
-              </div>
+              <div className="mt-4">{row.desc}</div>
             </div>
           </div>
           <div className=" w-[80%] h-96 bg-white rounded-3xl mt-8 drop-shadow-lg justify-end flex overflow-hidden">
             <div className="w-[53.1%] h-full px-8 py-4 ">
               <div className="w-full uppercase">collection</div>
               <div className="uppercase text-2xl mt-4 font-semibold">
-                69 best healthy foods recipes
+                {row.value} best {row.category} foods recipes
               </div>
-              <div className="mt-4">
-                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                Lorem ipsum
-              </div>
+              <div className="mt-4">{row.desc}</div>
             </div>
             <img
-              src="https://www.eatthis.com/wp-content/uploads/sites/4/2021/05/healthy-foods.jpg?quality=82&strip=1"
+              src={row.image}
               className="w-[46.9%] h-full object-cover object-center"
             />
           </div>
         </div>
       </div>
     </div>
-  );
+  ));
 }
