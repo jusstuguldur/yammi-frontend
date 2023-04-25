b594010245ca34605d93e97aa677831e7dc1692b
Irmuunn <irmuunselenge85@gmail.com>
2023-04-25 09:31:02 +0800

    muun

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,23 @@
-export default function Login() {
+import { useState } from 'react';
+export default function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
   return (
-    <>
-      <div className="w-screen h-screen bg-beige flex justify-center items-center">
+    <div className="w-screen h-screen bg-beige flex justify-center items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1440 320"
@@ -24,8 +40,34 @@ export default function Login() {
             d="M0,256L30,245.3C60,235,120,213,180,192C240,171,300,149,360,138.7C420,128,480,128,540,133.3C600,139,660,149,720,138.7C780,128,840,96,900,85.3C960,75,1020,85,1080,90.7C1140,96,1200,96,1260,80C1320,64,1380,32,1410,16L1440,0L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
           ></path>
         </svg>
-        <div className="bg-white w-[50%] h-[65%] drop-shadow-lg rounded-3xl"></div>
+        <div className="bg-white w-[50%] h-[65%] drop-shadow-lg rounded-3xl">
+        <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+        </div>
+        
       </div>
-    </>
+    
   );
 }
