ee6f6237da76fd1964d5e195f1ce49f29cd0da16
Irmuunn <irmuunselenge85@gmail.com>
2023-05-02 18:08:33 +0800

    search

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,9 +1,59 @@
-import Navbar from "./components/navbar";
+import Head from 'next/head';
 
-export default function Search() {
+export default function ComingSoon() {
   return (
-    <div>
-      <Navbar></Navbar>
+    <div className="container">
+      <Head>
+        <title>Coming Soon</title>
+        <meta name="description" content="Our website is coming soon" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Тун удахгүй</h1>
+        <p>Шинэчлэлт хийгдэж буй тул түр хүлээнэ үү!</p>
+      </main>
+
+      <footer>
+        <p>ITech © {new Date().getFullYear()}
+        </p>
+      </footer>
+
+      <style jsx>{`
+        .container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          min-height: 100vh;
+          padding: 0 1rem;
+        }
+
+        main {
+          padding: 5rem 0;
+          text-align: center;
+        }
+
+        h1 {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        p {
+          font-size: 1.5rem;
+          line-height: 1.5;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-top: 1px solid #eaeaea;
+        }
+      `}</style>
     </div>
   );
 }
